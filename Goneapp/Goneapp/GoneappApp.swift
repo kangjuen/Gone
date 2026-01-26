7cc44895326be5fd102faa62e59145e6335abc9b
kangjuen <00112233pq@gmail.com>
2026-01-26 23:20:20 +0900

    Chore: Xcode 프로젝트 초기 세팅

---
+++ Goneapp/Goneapp/GoneappApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoneappApp.swift
+//  Goneapp
+//
+//  Created by 강주은 on 1/26/26.
+//
+
+import SwiftUI
+
+@main
+struct GoneappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
